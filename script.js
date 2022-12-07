8417454f7e6ea119c4a38bfb8de130e316050d2b
TheUnknownMonkey <miha.puc@gmail.com>
2022-12-07 23:10:16 +0100

    MVP done

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 let game = true;
 const grid = document.getElementById("grid");
-let squares = [];
+const eight = document.getElementById("8x8");
+const sixteen = document.getElementById("16x16");
+const thirtytwo = document.getElementById("32x32");
 
+function columnNumber(number){
+    let columns = "";
+    let squares = [];
+    grid.innerHTML = '';
 
-
-for (let i=0; i <64 ;i++){
-    squares.push(document.createElement("div"));
-    grid.appendChild(squares[i]);
-    squares[i].addEventListener("click", color(i));
-    function color(i){
-        squares[i].style.border="2px solid red";
-        return
+    for (let i=0; i <number ;i++){
+       columns+="1fr "
     }
+
+    document.getElementById("grid").style.gridTemplateColumns=columns;
+
+    for (let i=0; i <number*number ;i++){
+        squares.push(document.createElement("div"));
+        grid.appendChild(squares[i]);
+        squares[i].addEventListener("mouseover",function color(){
+            squares[i].style.background="black";
+        },false);
+       
+    
+    }
+
 
 }
 
+columnNumber(16)
+
+// function buttonClick(){
+
+// }
 
 
+
+
+eight.addEventListener("click", function eight(){
+    columnNumber(8);
+},false);
+
+sixteen.addEventListener("click", function eight(){
+    columnNumber(16);
+},false);
+thirtytwo.addEventListener("click", function eight(){
+    columnNumber(32);
+},false);
 
 // while(game==true){
 
